6389f593fd76d5ef53d3e6717c11c6d0b37dcf53
chchch <charles.li@cnrs.fr>
2024-01-30 15:36:00 +0100

    what about now

--- js/alignment.mjs
+++ js/alignment.mjs
@@ -45,13 +45,13 @@ const alignmentXSLT = `<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.or
     <xsl:element name="td">
         <xsl:attribute name="data-n">
             <xsl:variable name="n" select="@n"/>
-            <xsl:variable name="abbr" select="//x:witness[@xml:id='$n']/x:abbr[@type='siglum']"/>
+            <xsl:variable name="abbr" select="//x:witness[@xml:id=$n]/x:abbr[@type='siglum']"/>
             <xsl:choose>
                 <xsl:when test="$abbr">
                     <xsl:value-of select="$abbr"/>
                 </xsl:when>
                 <xsl:otherwise>
-                    <xsl:value-of select="@n"/>
+                    <xsl:value-of select="$n"/>
                 </xsl:otherwise>
             </xsl:choose>
         </xsl:attribute>
