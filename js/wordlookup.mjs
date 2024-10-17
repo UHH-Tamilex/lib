bb7b3c53683a771b0397a8bf16c2388f1d00ab08
chchch <charles.li@cnrs.fr>
2024-10-17 11:02:01 +0200

    bugfix

--- js/wordlookup.mjs
+++ js/wordlookup.mjs
@@ -40,7 +40,7 @@ const wordLookup = async (word) => {
 const formatCitations = citations => {
     return '<table><tbody>' + citations.map(c => {
         const link = c.line ? 
-            encodeURIComponent(`c.filename?highlight=[id="${c.siglum}"] l:nth-of-type[${c.line}]`)
+            encodeURIComponent(`${c.filename}?highlight=[id="${c.siglum}"]%20l:nth-of-type[${c.line}]`)
             : c.filename;
 
         return `<tr>
