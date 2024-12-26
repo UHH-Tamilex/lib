8c1c8cb891a28eea1b2254f92c95f3d20dbc6292
chchch <charles.li@cnrs.fr>
2024-12-26 11:36:47 +0100

    fix hyphenation bug

--- js/ta-Latn.mjs
+++ js/ta-Latn.mjs
@@ -3,9 +3,9 @@ const hyphenation_ta_Latn = {
     leftmin : 2,
     rightmin : 2,
     patterns : {
-        2 : "a1e1i1o1u1ē1í1ï1ō1ú1ü1ā1ī1ū1",
-        3 : "2b_2c_2d_2g_2h_2j_2k_2l_2m_2n_2p_2r_2s_2t_2v_2y_2ñ_2ś_2ḍ_2ḥ12ḫ12ṁ12ṃ12ṅ_2ṇ_2ṭ_2ẖ1l̥1r̥1",
-        4 : "2bh_2ch_2dh_2gh_2jh_2kh_2m̐12ph_2rb_2rd_2rg_2rk_2rp_2rt_2rḍ_2rṭ_2th_2ḍh_2ṭh_a2i1a2u1a3ï1a3ü1l̥̄1r̥̄1"
+        2 : "a1e1i1o1u1ē1ō1ā1ī1ū1",
+        3 : "2c_2h_2k_2l_2m_2n_2p_2r_2s_2ṭ_2t_2y_2ñ_2ṟ_2ḻ_2ṉ_2ḷ_2ṣ_2j_2ḥ12ṃ12ṅ_2ṇ_2ḵ1",
+        4 : "a2i1a2u12kṣ_2ṭc_"
     },
 };
 
