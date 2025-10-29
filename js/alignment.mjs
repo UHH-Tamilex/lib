0bdde41982a6383fa66edb45f404ea3029bb4220
chchch <chuck@mixed-metaphors.com>
2025-10-29 09:56:54 +0100

    bugfix

--- js/alignment.mjs
+++ js/alignment.mjs
@@ -36,7 +36,7 @@ const alignmentXSLT = `<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.or
     <xsl:element name="tr">
         <xsl:element name="th">
             <xsl:variable name="n" select="../@n"/>
-            <xsl:variable name="abbr" select="//x:witness[@xml:id=$n]//x:abbr[@type='siglum']"/>
+            <xsl:variable name="abbr" select="//x:witness[@xml:id=$n]/x:abbr"/>
             <xsl:choose>
                 <xsl:when test="$abbr">
                     <xsl:value-of select="$abbr"/>
